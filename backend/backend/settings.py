5ccc6b1bf39c0d3ade7237415b85753c657dd656
Marco <marco.seraglini@stud.unifi.it>
2023-06-12 11:55:53 +0200

    commit

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -176,6 +176,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
@@ -184,7 +185,6 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = BASE_DIR / 'static/images'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
